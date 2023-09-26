1b4c0c777e26af78ab219d7b8723952cdf50f06a
Ignacio Vargas <ignaevc@gmail.com>
2023-09-26 14:29:02 -0600

    Cambiar declaración de apply_filter y rellenar función periódica de cortex

--- filtro/src/filter.c
+++ filtro/src/filter.c
@@ -3,10 +3,11 @@
 #include "memory.h"
 #include "util.h"
 
-void apply_filter(uint8_t *pixels, uint8_t *out_buf, size_t num_pixels,
-                  size_t image_w, size_t image_h) {
-  UNUSED(pixels);
+void apply_filter(uint8_t *image, uint8_t *out_buf, size_t start_idx,
+                  size_t num_pixels, size_t image_w, size_t image_h) {
+  UNUSED(image);
   UNUSED(out_buf);
+  UNUSED(start_idx);
   UNUSED(num_pixels);
   UNUSED(image_w);
   UNUSED(image_h);
--- filtro/src/process.c
+++ filtro/src/process.c
@@ -2,6 +2,7 @@
 #include "filter.h"
 #include "memory.h"
 #include "util.h"
+#include <pthread.h>
 
 static int soft_filter_px_count = 0;
 void timer_isr_soft_core() {
@@ -25,13 +26,13 @@ void timer_isr_soft_core() {
   if (soft_filter_px_count == 0 &&
       (unfiltered_decrypted_px_count > 2 * header->image_w)) {
     num_pixels_to_filter = unfiltered_decrypted_px_count;
-  } else if (soft_filter_px_count > 0) {
+  } else if ((soft_filter_px_count > 0) || header->decrypt_done) {
     // Si ya empezó filtrado no esperar
     num_pixels_to_filter = unfiltered_decrypted_px_count;
   }
 
-  apply_filter((uint8_t *)sdram + DECRYPTED_IMAGE_SECTOR + soft_filter_px_count,
-               (uint8_t *)sdram + FILTERED_IMAGE_SECTOR + soft_filter_px_count,
+  apply_filter((uint8_t *)sdram + DECRYPTED_IMAGE_SECTOR,
+               (uint8_t *)sdram + FILTERED_IMAGE_SECTOR, soft_filter_px_count,
                num_pixels_to_filter, header->image_w, header->image_h);
   soft_filter_px_count += num_pixels_to_filter;
 
@@ -40,20 +41,52 @@ void timer_isr_soft_core() {
   if (soft_filter_px_count == header->filter_hps_start) {
     header->nios_filter_done = true;
   }
+  // Apagar ISR
+  pthread_exit(0);
 }
 
 static int hard_filter_px_count = 0;
 void proceso_periódico_hard_core() {
   // fijarse en header de programa de sdram
   struct shared_header *header = get_header();
-  UNUSED(header);
 
   // hay pixeles pa procesar por parte del HPS?
+  if (header->decrypt_px_count < header->filter_hps_start) {
+    return;
+  }
   // si sí, procesa lo que hay
 
-  // si no, si no ha terminado hace yield
+  // si hay más de dos filas descifradas, aplica filtro hasta agotar los pixeles
+  // descifrados que se pueden filtrar
+  int num_pixels_to_filter = 0;
 
-  // si ya termino, guarda ambas imagenes a sd
+  int unfiltered_decrypted_px_count = header->decrypt_px_count -
+                                      header->filter_hps_start -
+                                      hard_filter_px_count;
+
+  // Asegurar que hayan 2 filas descifradas antes de empezar
+  if (hard_filter_px_count == 0 &&
+      ((header->filter_hps_start + unfiltered_decrypted_px_count) >
+       2 * header->image_w)) {
+    num_pixels_to_filter = unfiltered_decrypted_px_count;
+  } else if ((hard_filter_px_count > 0) || header->decrypt_done) {
+    // Si ya empezó filtrado no esperar
+    num_pixels_to_filter = unfiltered_decrypted_px_count;
+  }
+
+  apply_filter((uint8_t *)sdram + DECRYPTED_IMAGE_SECTOR,
+               (uint8_t *)sdram + FILTERED_IMAGE_SECTOR,
+               header->filter_hps_start + hard_filter_px_count,
+               num_pixels_to_filter, header->image_w, header->image_h);
+  hard_filter_px_count += num_pixels_to_filter;
+
+  // si ya llego a filter_hps_start deja de filtrar y apaga el
+  // ISR. Setea nios_filter_done
+  if (hard_filter_px_count == header->filter_hps_start) {
+    header->hps_filter_done = true;
+  }
+  // Apagar ISR
+  pthread_exit(0);
 }
 
 void main_loop_soft_core() {
