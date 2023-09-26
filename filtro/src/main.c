45e5ba318b96c7725f843e5867d34cce150a882a
Ignacio Vargas <ignaevc@gmail.com>
2023-09-26 16:58:57 -0600

    Quitar print de hello world

--- filtro/src/main.c
+++ filtro/src/main.c
@@ -10,10 +10,6 @@ int main(int argc, char **argv) {
 
   struct arguments args = parse_args(argc, argv);
 
-  printf("Hello World!\r\n");
-  printf("%% Cortex: %f\r\n", args.percent_cortex);
-  printf("%% Qsys: %f\r\n", args.percent_qsys);
-
   init_memory(args);
 
   start_timer(timer_isr_soft_core);
